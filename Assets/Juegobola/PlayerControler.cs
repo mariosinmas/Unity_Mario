36fd9b40a9fc9903430f6d7fcb7391b649cdbdc0
mariosinmas <36511003+mariosinmas@users.noreply.github.com>
2018-02-15 18:15:35 +0100

    Añadido clase 1

---
+++ Assets/Juegobola/PlayerControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+	Rigidbody rb;
+	public float velocidad = 10f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mHorizontal = Input.GetAxis ("Horizontal");
+		float mVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movimiento = new Vector3 (mHorizontal,0,mVertical);
+		rb.AddForce (movimiento * velocidad * Time.deltaTime);	}
+}
---
+++ Assets/Teoria/T01HolaMundo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T01HolaMundo : MonoBehaviour {
+	
+	// mi nombre
+	string nombre = "Mario";
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log ("Hola " + nombre);
+
+		// Tipos de datos
+
+		// Enteros; int
+
+		int numero = 13;
+		int numero2 = 34;
+		int numero3 = 3444;
+
+		// int numero = 13, numero2 = 34, numero = 3444;
+
+		// Numeros reales: float -> Numeros decimales positivos y negativos
+
+		float nDecimal = 3.237683712f;
+
+		// Cadenas de texto: string
+
+		string mensaje = "Hola caracola";
+
+		// Valores lógicos: bool
+
+		// true == verdadero
+
+		// false == falso
+
+		bool decision = false;
+
+		Debug.Log (numero);
+		Debug.Log (numero2);
+		Debug.Log (numero3);
+	}
+	
+
+}
